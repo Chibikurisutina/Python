0fe5aa0803029bbc5926b652976704bce5f87387
Cristina Rosa <122946425+Chibikurisutina@users.noreply.github.com>
2024-11-18 17:29:24 -0300

    dicionário

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def exibir_nome_do_programa():
     print("""
     SABOR EXPRESS
     """)
-#menu
+#função menu
 def exibir_opcoes():
     print('1. Cadastrar Restaurante')
     print('2. Listar Restaurante')
@@ -58,6 +58,21 @@ def listar_restaurantes():
        print(f'-{nome_restaurante} | {categoria} | {ativo}')
     voltar_ao_menu_principal()
 
+def alternar_estado_restaurante():
+    exibir_subtitulo('Alterando estado do restaurante')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja alterar o estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if nome_do_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome_do_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+            print(mensagem)
+
+    if not restaurante_encontrado:
+        print('O restaurante não foi encontrado')
+
 def escolher_opcoes():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
@@ -67,7 +82,7 @@ def escolher_opcoes():
         elif opcao_escolhida == 2:
             listar_restaurantes()
         elif opcao_escolhida == 3:
-            print('Ativar restaurante')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4:
             finalizar_app() #chamando a função
         else:
---
+++ exercicio-dicionario.py
@@ -0,0 +1,2 @@
+pessoa = [{'nome':'Daiane','idade':30,'cidade':'Campos'}]
+pessoa['cidade']='Rio de Janeiro'
