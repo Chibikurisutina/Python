c39c9d43e2e829bc815f1e41afe41a3d798394ac
Cristina Rosa <122946425+Chibikurisutina@users.noreply.github.com>
2024-11-19 17:16:35 -0300

    continuação exercícios

--- exercicio-dicionario.py
+++ exercicio-dicionario.py
@@ -1,2 +1,16 @@
-pessoa = [{'nome':'Daiane','idade':30,'cidade':'Campos'}]
-pessoa['cidade']='Rio de Janeiro'
+pessoa = [{
+    'nome':'Daiane',
+    'idade':30,
+    'cidade':'Campos'}]
+pessoa['cidade']='Rio de Janeiro' #alterar item do dicionário
+pessoa['profissão']='artista' #adicionar campo no dicionário
+pessoa.pop('idade') #remover campo
+
+print(pessoa)
+
+if 'nome' in pessoa: # ver se chave existe no dicionário 
+    print('Sim, a chave nome esta no dicionário pessoa')
+
+#dicionário de numeros 1 a 5 e seus ²
+numeros_quadrados = {i: i**2 for i in range (1,6)}
+print(numeros_quadrados)
